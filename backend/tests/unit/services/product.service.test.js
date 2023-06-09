f09bfca1901752a02f49262900895ed4e658dbe7
cunhavcs <cunha.vcs@gmail.com>
2023-06-09 19:38:48 -0300

    Req. 01 - refatora testes da camada service

--- backend/tests/unit/services/product.service.test.js
+++ backend/tests/unit/services/product.service.test.js
@@ -1,22 +1,22 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
 
-const connection = require('../../../src/models/connection');
 const productModel = require('../../../src/models/product.model');
+const productService = require('../../../src/services/product.service');
 const { mockedProducts } = require('../mocks/product.mock');
 
-describe('Testes unitários da camada model de "products"', function () {
+describe('Testes unitários da camada service de "products"', function () {
   afterEach(sinon.restore);
 
   it('getAllProducts retorna todos os produtos cadastrados', async function () {
-    sinon.stub(connection, 'execute').resolves([mockedProducts]);
-    const result = await productModel.getAllProducts();
+    sinon.stub(productModel, 'getAllProducts').resolves(mockedProducts);
+    const result = await productService.getAllProducts();
     expect(result).to.be.deep.equal(mockedProducts);
   });
 
   it('getProductById retorna o produto correspondente ao id informado', async function () {
-    sinon.stub(connection, 'execute').resolves([[mockedProducts[0]]]);
-    const result = await productModel.getProductById(1);
+    sinon.stub(productModel, 'getProductById').resolves(mockedProducts[0]);
+    const result = await productService.getProductById(1);
     expect(result).to.be.deep.equal(mockedProducts[0]);
   });
 });
