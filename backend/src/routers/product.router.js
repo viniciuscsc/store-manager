c1296108612ab5aec78c5ef6c3c56e69825f6519
cunhavcs <cunha.vcs@gmail.com>
2023-06-09 21:08:59 -0300

    Req. 01 - refatora middleware existingProductValidation

--- backend/src/routers/product.router.js
+++ backend/src/routers/product.router.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 
 const productController = require('../controllers/product.controller');
+const existingProductValidation = require('../middlewares/existingProductValidation');
 
 const router = Router();
 
 router.get('/', productController.getAllProducts);
-router.get('/:id', productController.getProductById);
+router.get('/:id', existingProductValidation, productController.getProductById);
 
 module.exports = router;
