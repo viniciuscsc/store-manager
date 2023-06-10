d1abe1019f8d111c12fac423a8c487916f7f19bc
cunhavcs <cunha.vcs@gmail.com>
2023-06-09 21:20:06 -0300

    Req. 02 - refatora funções de sale.model

---
+++ backend/src/models/sale.model.js
@@ -0,0 +1,37 @@
+const connection = require('./connection');
+
+const getAllSales = async () => {
+  const [result] = await connection.execute(
+    `SELECT
+      s.id AS saleId,
+      s.date,
+      sp.product_id AS productId,
+      sp.quantity
+    FROM sales AS s
+    INNER JOIN sales_products AS sp
+      ON s.id = sp.sale_id
+    ORDER BY saleId, productId`,
+  );
+  return result;
+};
+
+const getSaleById = async (saleId) => {
+  const [[result]] = await connection.execute(
+    `SELECT
+      s.date,
+      sp.product_id AS productId,
+      sp.quantity
+    FROM sales AS s
+    INNER JOIN sales_products AS sp
+      ON s.id = sp.sale_id
+    WHERE s.id = ?
+    ORDER BY sale_id, productId`,
+    [saleId],
+  );
+  return result;
+};
+
+module.exports = {
+  getAllSales,
+  getSaleById,
+};
