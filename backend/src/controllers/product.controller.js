4c2106a6a264d298d244369730c565975f87ca8d
cunhavcs <cunha.vcs@gmail.com>
2023-06-10 17:00:40 -0300

    Req. 03 - refatora as funções em product.controller

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -1,20 +1,28 @@
 const productService = require('../services/product.service');
 
+const errorMap = {
+  INVALID_VALUE: 422,
+  PRODUCT_NOT_FOUND: 404,
+};
+
 const getAllProducts = async (_req, res) => {
-  const result = await productService.getAllProducts();
-  return res.status(200).json(result);
+  const { type, message } = await productService.getAllProducts();
+  if (type) return res.status(errorMap[type]).json(message);
+  return res.status(200).json(message);
 };
 
 const getProductById = async (req, res) => {
   const { id } = req.params;
-  const result = await productService.getProductById(+id);
-  return res.status(200).json(result);
+  const { type, message } = await productService.getProductById(+id);
+  if (type) return res.status(errorMap[type]).json(message);
+  return res.status(200).json(message);
 };
 
 const registerProduct = async (req, res) => {
   const productData = req.body;
-  const result = await productService.registerProduct(productData);
-  return res.status(201).json(result);
+  const { type, message } = await productService.registerProduct(productData);
+  if (type) return res.status(errorMap[type]).json(message);
+  return res.status(201).json(message);
 };
 
 module.exports = {
