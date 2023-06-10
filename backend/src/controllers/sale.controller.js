927321dc2b42095cc7bf65348728985779562d66
cunhavcs <cunha.vcs@gmail.com>
2023-06-10 17:40:03 -0300

    Req. 03 - refatora funções de sale.controller

--- backend/src/controllers/sale.controller.js
+++ backend/src/controllers/sale.controller.js
@@ -1,14 +1,20 @@
 const saleService = require('../services/sale.service');
 
+const errorMap = {
+  SALE_NOT_FOUND: 404,
+};
+
 const getAllSales = async (_req, res) => {
-  const result = await saleService.getAllSales();
-  return res.status(200).json(result);
+  const { type, message } = await saleService.getAllSales();
+  if (type) return res.status(errorMap[type]).json(message);
+  return res.status(200).json(message);
 };
 
 const getSaleById = async (req, res) => {
   const { id } = req.params;
-  const result = await saleService.getSaleById(+id);
-  return res.status(200).json(result);
+  const { type, message } = await saleService.getSaleById(+id);
+  if (type) return res.status(errorMap[type]).json({ message });
+  return res.status(200).json(message);
 };
 
 module.exports = {
