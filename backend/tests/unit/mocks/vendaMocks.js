56ea6a7ddb37fd7daadf82a5550b8351a9669096
cunhavcs <cunha.vcs@gmail.com>
2023-06-20 00:15:00 -0300

    Req. 04: parte dos testes de vendaModel.js

---
+++ backend/tests/unit/mocks/vendaMocks.js
@@ -0,0 +1,30 @@
+const vendasMock = [
+  {
+    saleId: 1,
+    date: '2023-06-20T02:27:09.000Z',
+    productId: 1,
+    quantity: 5,
+  },
+  {
+    saleId: 1,
+    date: '2023-06-20T02:27:09.000Z',
+    productId: 2,
+    quantity: 10,
+  },
+  {
+    saleId: 2,
+    date: '2023-06-20T02:27:09.000Z',
+    productId: 3,
+    quantity: 15,
+  },
+];
+
+const dadosVendaMock = [
+  { productId: 1, quantity: 1 },
+  { productId: 2, quantity: 5 },
+];
+
+module.exports = {
+  vendasMock,
+  dadosVendaMock,
+};
---
+++ backend/tests/unit/models/vendaModel.test.js
@@ -0,0 +1,61 @@
+const { expect, use } = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+
+const connection = require('../../../src/models/connection');
+const vendaModel = require('../../../src/models/vendaModel');
+
+const {
+  vendasMock,
+  dadosVendaMock,
+} = require('../mocks/vendaMocks');
+
+use(sinonChai);
+
+describe('Testes de vendaModel', function () {
+  afterEach(function () {
+    sinon.restore();
+  });
+
+  it('A função obterVendas retorna todas as vendas', async function () {
+    sinon.stub(connection, 'execute').resolves([vendasMock]);
+
+    const vendas = await vendaModel.obterVendas();
+
+    expect(vendas).to.be.deep.equal(vendasMock);
+  });
+
+  it(
+    'A função obterVendaPorId retorna a venda correspondente ao id informado',
+    async function () {
+      sinon.stub(connection, 'execute').resolves([vendasMock[2]]);
+
+      const venda = await vendaModel.obterVendaPorId(2);
+
+      expect(venda).to.be.deep.equal(vendasMock[2]);
+    },
+  );
+
+  it(
+    'A função cadastrarVenda cadastra uma venda no database e retorna o insertId',
+    async function () {
+      sinon.stub(connection, 'execute').resolves([{ insertId: 3 }]);
+
+      const idVenda = await vendaModel.cadastrarVenda(dadosVendaMock);
+
+      expect(idVenda).to.be.equal(3);
+    },
+  );
+
+  it('A função atualizarVenda atualiza uma venda no database', async function () {
+    sinon.stub(connection, 'execute').resolves([[{ date: vendasMock[0].date }]]);
+
+    const dataVenda = await vendaModel.atualizarVenda(
+      vendasMock[0].saleId,
+      vendasMock[0].productId,
+      { quantity: 10 },
+    );
+
+    expect(dataVenda).to.be.equal(vendasMock[0].date);
+  });
+});
